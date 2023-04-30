01b922c754d4e55636eba16cf29377ae18e8ea8c
Kudz <kudzayibamhare@gmail.com>
2023-04-30 22:41:03 +0200

    Created Kirby and made art

--- art.py
+++ art.py
@@ -1,1 +1,12 @@
-print('hello, turtle!')
+import turtle
+
+kirby = turtle.Turtle()
+screen = turtle.Screen().bgcolor('black')
+kirby.speed(0)
+for i in range(360):
+    kirby.color('yellow')
+    kirby.left(1)
+    kirby.fd(1)
+    for j in range(2):
+        kirby.left(2)
+        kirby.circle(100)
